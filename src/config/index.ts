c9717ee37e835c1b01c33f830eb48fe6d2a3fbed
Romullo <romullocordeiro@gmail.com>
2021-06-19 16:25:44 -0300

    feat: configure aws-sdk

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: path.join(__dirname, '..', '..', '.env') });
 
-const { DB_URI } = process.env;
+const {
+  DB_URI,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_REGION,
+  AWS_BUCKET,
+} = process.env;
 
 const SERVER_PORT = Number(process.env.SERVER_PORT);
 
-export { SERVER_PORT, DB_URI };
+export {
+  SERVER_PORT,
+  DB_URI,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_REGION,
+  AWS_BUCKET,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
-import 'reflect-metadata';
-
-import express from 'express';
+import { config } from 'aws-sdk';
 import cors from 'cors';
-import { SERVER_PORT, DB_URI } from './config';
-import { routes } from './routes';
+import express from 'express';
 import mongoose from 'mongoose';
+import 'reflect-metadata';
+import {
+  AWS_ACCESS_KEY_ID,
+  AWS_REGION,
+  AWS_SECRET_ACCESS_KEY,
+  DB_URI,
+  SERVER_PORT,
+} from './config';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded());
 routes(app);
 mongoose
   .connect(DB_URI)
@@ -20,6 +26,12 @@ mongoose
   .catch(err => {
     console.log(err);
   });
+
+config.update({
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  region: AWS_REGION,
+});
 
 app.listen(SERVER_PORT, async () => {
   console.log(`Server started on ${SERVER_PORT}`);
